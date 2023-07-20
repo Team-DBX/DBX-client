a2f7740b8e630b538196656cd9288abb0bfc14ef
koreaNerd <pksksk0147@gmail.com>
2023-07-21 01:01:33 +0900

    feat: Login컴포넌트 초기생성 커밋

--- src/App.jsx
+++ src/App.jsx
@@ -1,5 +1,14 @@
 import React from "react";
+import { Routes, Route } from "react-router-dom";
+import Login from "./components/Login";
 
-export default function App() {
-	return <div>App</div>;
+function App() {
+	return (
+		<Routes>
+			<Route path="/" exact element={<Login />} />
+			<Route path="/login" element={<Login />} />
+		</Routes>
+	);
 }
+
+export default App;
